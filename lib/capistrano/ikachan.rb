3e818d9c180843eb4c10d8817a5719d22dc7149f
linyows <linyows@gmail.com>
2014-06-12 15:34:52 +0900

    avoid error "undefined method `namespace`"

--- lib/capistrano/ikachan.rb
+++ lib/capistrano/ikachan.rb
@@ -1,9 +1,3 @@
-load File.expand_path("../tasks/ikachan.rake", __FILE__)
-
+require "capistrano/all"
 require "capistrano/ikachan/version"
-
-module Capistrano
-  module Ikachan
-    # Your code goes here...
-  end
-end
+load File.expand_path("../tasks/ikachan.rake", __FILE__)
